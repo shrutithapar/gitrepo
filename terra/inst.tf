c3524f7425dac4eb0cb3f65555cdf6efc8c15813
EC2 Default User <ec2-user@ip-172-31-31-62.us-east-2.compute.internal>
2020-05-13 17:29:18 +0000

    main.tf

--- terra/inst.tf
+++ terra/inst.tf
@@ -4,8 +4,8 @@ provider "aws" {
 resource "aws_instance" "task1" {
   ami = "ami-0c64dd618a49aeee8"
   instance_type = "t2.micro"
-  key_name = "key"
+  key_name = "shruti_AWS"
   tags = {
    Name = "linux_server"
-  }
-  }
+}
+}
