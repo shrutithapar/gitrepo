f146ae26f24864fd7725764019fdb1bd1f07e47a
EC2 Default User <ec2-user@ip-172-31-31-62.us-east-2.compute.internal>
2020-05-13 12:43:18 +0000

    main.tf

---
+++ terra/inst.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "task1" {
+  ami = "ami-0c64dd618a49aeee8"
+  instance_type = "t2.micro"
+  key_name = "key"
+  tags = {
+   Name = "linux_server"
+  }
+  }
