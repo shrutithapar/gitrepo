ed5a27748f29735d3c71c53165f923ac097adf98
EC2 Default User <ec2-user@ip-172-31-31-62.us-east-2.compute.internal>
2020-05-13 10:12:04 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
   access_key = "${var.access_key}"
-  secret_key = "${var.secet_key}"
+  secret_key = "${var.secret_key}"
 }
 resource "aws_key_pair" "deployer" {
   key_name = "samplekey"
