06eee8568288b1ba237dc7ffc4f09336a78e29a7
EC2 Default User <ec2-user@ip-172-31-31-62.us-east-2.compute.internal>
2020-05-13 07:40:18 +0000

    var file

--- var.tf
+++ var.tf
@@ -1,59 +1,29 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "ami" {
+variable "access_keyf" {}
+variable "secret_keyf" {}
+variable "amif" {
   default = "ami-0c64dd618a49aeee8"
 }
-variable "instance_type" {
+variable "instance_typef" {
   default = "t2.micro"
 }
-variable "user" {
+variable "userf" {
   default = "ec2-user"
 }
-variable "instag" {
+variable "instagf" {
   default = "inst-1"
 }
-variable "prot" {
+variable "protf" {
   default = "tcp"
 }
-variable "sgtag" {
+variable "sgtagf" {
   default = "secgrp"
 }
-variable "p80" {
+variable "p80f" {
   default = 80
 }
-variable "p22" {
+variable "p22f" {
   default = 22
 }
-variable "vpc" {
+variable "vpcf" {
   default = "vpc-7249b619"
 }
-variable "access_key" {}
-variable "secret_key" {}
-variable "ami" {
-  default = "ami-0c64dd618a49aeee8"
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "user" {
-  default = "ec2-user"
-}
-variable "instag" {
-  default = "inst-1"
-}
-variable "prot" {
-  default = "tcp"
-}
-variable "sgtag" {
-  default = "secgrp"
-}
-variable "p80" {
-  default = 80
-}
-variable "p22" {
-  default = 22
-}
-variable "vpc" {
-  default = "vpc-7249b619"
-}
-
