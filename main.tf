0ac96a5279f15b6d23b88ba22a2ac1470aedad3b
shrutithapar <shrutithapar15@gmail.com>
2020-05-14 11:55:19 +0000

    one1

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
 }
-resource "aws_key_pair" "deployer" {
-  key_name = "samplekey"
-  public_key = "${file("/home/ec2-user/id_rsa.pub")}"
-}
-resource "aws_instance" "instance" {
-  ami = "ami-0c64dd618a49aeee8"
-  instance_type = "t2.micro"
-  key_name = "shruti_AWS"
-  tags = {
-   Name = "inst"
-}
+resource "aws_instance" "web" {
+  ami           = "ami-0c64dd618a49aeee8"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
