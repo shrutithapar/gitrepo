c51bc718035f33d93dedaee07a821cd8c7d1cdad
shrutithapar <shrutithapar15@gmail.com>
2020-05-15 06:25:18 +0000

    one1

--- main.tf
+++ main.tf
@@ -7,47 +7,47 @@ resource "aws_key_pair" "deployer" {
   public_key = file("id_rsa.pub")
 }
 resource "aws_instance" "task1" {
-  ami             = "ami-0c64dd618a49aeee8"
-  instance_type   = "${var.instance_type}"
+  ami             = "var.ami"
+  instance_type   = "var.instance_type"
   key_name        = "samplekey"
   security_groups = ["${aws_security_group.secgrp.name}"]
   user_data = "${file("httpd.sh")}"
   tags = {
-    Name = "${var.instag}"
+    Name = "var.instag"
   }
 }
 resource "aws_security_group" "secgrp" {
   name        = "secgrp"
   description = "Allow inbound and outbound traffic"
-  vpc_id      = "${var.vpc}"
+  vpc_id      = "var.vpc"
   ingress {
     description = "inbound rule"
-    from_port   = "${var.p80}"
-    to_port     = "${var.p80}"
-    protocol    = "${var.prot}"
+    from_port   = "var.p80"
+    to_port     = "var.p80"
+    protocol    = "var.prot"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "inbound rule"
-    from_port   = "${var.p22}"
-    to_port     = "${var.p22}"
-    protocol    = "${var.prot}"
+    from_port   = "var.p22"
+    to_port     = "var.p22"
+    protocol    = "var.prot"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = "${var.p80}"
-    to_port     = "${var.p80}"
-    protocol    = "${var.prot}"
+    from_port   = "var.p80"
+    to_port     = "var.p80"
+    protocol    = "var.prot"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = "${var.p22}"
-    to_port     = "${var.p22}"
-    protocol    = "${var.prot}"
+    from_port   = "var.p22"
+    to_port     = "var.p22"
+    protocol    = "var.prot"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "${var.sgtag}"
+    Name = "var.sgtag"
   }
 }
